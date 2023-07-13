73058d5dfc258515893f9fb3127899876dfd762a
edu290386 <eduardo.torres@outlook.com>
2023-07-12 22:53:36 -0500

    vercel solved

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,7 +9,7 @@ import Avatar1 from '../assets/Avatar1.webp'
 import Avatar2 from '../assets/Avatar2.webp'
 import Avatar3 from '../assets/Avatar3.webp'
 import Avatar4 from '../assets/Avatar4.webp'
-import Avatar5 from '../assets/Avatar5.png'
+import Avatar5 from '../assets/Avatar5.webp'
 
 const options = [
   { value:Avatar1, label:'Avatar 1' },
